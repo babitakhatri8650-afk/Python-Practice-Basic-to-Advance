8497118cd9ae8651f196ce440acc3ed4ac941768
Babita Khatri <babitakhatri8650@gmail.com>
2026-04-04 00:25:42 -0300

    Add files via upload

--- Q5.py
+++ Q5.py
@@ -1,26 +1,4 @@
-class vector:
-    def __inti__(self,x,y,z):
-        self.x=x
-        self.y=y
-        self.z=z
-
-    def __add__(self,other):
-        result=vector(self.x+other.x,self.y+other.y,self.z+other.z)
-        return result
-    
-    def __mul__(self,other):
-        result=vector(self.x*other.x,self.y*other.y,self.z*other.z)
-        return result
-
-
-    def __str__(self):
-        return f"vector({self.x}x,{self.y}y,{self.z}z)"
-v1=vector(2,4,5)
-v2=vector(3,5,6)
-v3=vector(4,6,7)
-
-print(v1+v2)
-print(v1*v2)
-        
-  
-        
+n=int(input("Enter number:"))
+table=[n*i for i in range(1,11)]
+with open ("table.txt","a") as f:
+    f.write(f"{str(table)}+\n")
