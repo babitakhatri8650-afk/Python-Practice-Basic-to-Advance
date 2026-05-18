a767e29633be7dc17d4c395a293e4e0c35ef9d5c
Babita Khatri <babitakhatri8650@gmail.com>
2026-05-18 11:52:29 -0300

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,12 @@
+
+#use of elif
+age=int(input("Enter your age\n"))
+
+if(age>18):
+    print("You can Drive")
+
+elif(age==18):
+    print("Let schedule an interview")
+
+else:
+    print("You can not Drive")
