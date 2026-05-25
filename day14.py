f425a58dcbc2001794b43d98b269c0cfa8db0461
Babita Khatri <babitakhatri8650@gmail.com>
2026-05-25 09:05:10 -0300

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,18 @@
+# Break
+for i in range(0,21):
+    print(i)
+    if i==11:
+        break
+
+# Continue
+for i in range(1,10):
+    if i==5:
+        continue
+    print(i)
+
+# PASS
+i=3
+
+if i==3:
+    pass #do nothing
+print("End of Program")
