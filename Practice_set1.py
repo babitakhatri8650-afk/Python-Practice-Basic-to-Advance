27d3728a9a839a5b1c06a99868f9bf43c81df9b5
Babita Khatri <babitakhatri8650@gmail.com>
2026-05-28 05:40:02 -0300

    Add files via upload

---
+++ Practice_set1.py
@@ -0,0 +1,48 @@
+#Q1 write a program that prints
+print("Hello ! Babita How are you?")
+
+#Q2 print a poem
+print("Twinkel,twinkel little star \n How I wonder what you are!")
+
+print('''Twinkel,twinkel little star 
+How I wonder what you are!
+''')
+#Q3 print your name ,age , height,is_student
+name="Babita"
+age=22
+height=160.5
+is_student=True
+
+print("The name of the students is "+name+ ".she is "+str(age)+ "she heights is"+str(height)+" and she  is a "+str(is_student))
+
+#Q4 typecasting
+num="45"
+print(type(num))
+print(int(num))
+
+#Q5 taking input from user
+food=input("What is your favorite food?\n")
+print("Wow i also like "+food)
+
+#Q6 simple calculator +,-,%,/
+a=int(input("Enter your first number"))
+b=int(input("Enter your second number"))
+
+#sum
+sum=a+b
+diff =a-b
+division=a/b
+modulo=a%b
+multi=a*b
+print(sum)
+print(diff)
+print(division)
+print(modulo)
+print(multi)
+
+#Q7 Use escape character
+print("my name is babita khatri /n I'm currently  prusuing my msc/I'm from Uttrakhand")
+
+#Q8 
+a=int(input("Enter your number:"))
+print("Square is",a**2)
