a7dae16ad249500fb9060ccf1aa79fb7f20b1eda
Babita Khatri <babitakhatri8650@gmail.com>
2026-05-29 13:42:19 -0300

    Add files via upload

---
+++ For_loop1.py
@@ -0,0 +1,15 @@
+#Q1
+num=int(input("Enter the number:"))
+for i in range(1,11):
+    print(num,"X",i,"= ",num*i)
+
+#Q2
+sum=0
+for i in range(1,101):
+    sum=sum+i
+print(sum)
+
+#Q3
+
+for i in range(1,5):
+    print("*"*i)
