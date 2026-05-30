d9c8cc956d19c391109dec0cb26b5cc000c32a5f
Babita Khatri <babitakhatri8650@gmail.com>
2026-05-30 07:45:49 -0300

    Add files via upload

---
+++ IF_Else_Conditional.py
@@ -0,0 +1,31 @@
+#Q1
+num=int(input("Enter your number: "))
+
+if(num>0):
+    print("Your number is possitive")
+
+elif(num<0):
+    print("Your number is negative")
+
+else:
+    print("Number is zero")
+
+#Q2
+
+age=int(input("Enter your age: "))
+
+if(age>=18):
+    print("You are eligible for vote")
+
+else:
+    print("Your are not eligible for vote")
+
+#Q3
+
+num1=int(input("Enter your number: "))
+
+if(num1%2==0):
+    print("Even")
+
+else:
+    print("Odd")
