6e8c457a90573999a2d88e35ec923de0c5fd059e
Babita Khatri <babitakhatri8650@gmail.com>
2026-06-04 07:58:01 -0300

    Add files via upload

--- Q4.py
+++ Q4.py
@@ -1,9 +1,5 @@
-def divisible5(n):
-    if n%5==0:
-        return True
-    return False
+text="I love python programming "
 
-a=[4,35,48735,234,22,77,8875]
-f=list(filter(divisible5,a))
-
-print(f)
+print(text.strip())
+print(text.title())
+print(text.count("o"))
